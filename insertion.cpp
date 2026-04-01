83bc7ce14603d1f727858ee32d4197e1c5d2a681
David Prince J <princed4vid77@gmail.com>
2026-04-01 21:54:40 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int key, j;
+
+    for(int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array:\n";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
